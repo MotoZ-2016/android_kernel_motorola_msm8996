mydatakeeper/qcacld-2.0-imx

qcacld-2.0: Add Support for FILS Association
Adds following support:
1) Add FILS IE's to assoc request.
2) AEAD encrypt/decrypt the assoc request/response frames for FILS
3) Process/Verify the Assoc response for the FILS params
4) Plumb Auth keys to firmware after successful FILS assoc
5) Update userspace with Auth key details

CRs-Fixed: 2033938
Change-Id: I57f6319cecdf10e08047b510321b885c3cd711b5

CORE/MAC/src/pe/lim/limSendManagementFrames.c
@@ -52,7 +52,7 @@
 #ifdef WLAN_FEATURE_11W
 #include "wni_cfg.h"
 #endif
-
+#include<crypto/aes.h>
 #ifdef WLAN_FEATURE_VOWIFI_11R
 #include "limFTDefs.h"
 #endif
@@ -64,7 +64,7 @@
 #if defined WLAN_FEATURE_VOWIFI
 #include "rrmApi.h"
 #endif
-
+#include "qdf_crypto.h"
 #include "wlan_qct_wda.h"
 
 #ifdef WLAN_FEATURE_FILS_SK
@@ -2074,6 +2074,10 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
     tANI_U8            *pIe = NULL;
     tANI_U32            ieLen = 0;
     tANI_U32            fixed_param_len = 0;
+#ifdef WLAN_FEATURE_FILS_SK
+    uint32_t aes_block_size_len = 0;
+    VOS_STATUS vos_status;
+#endif
 
     if(NULL == psessionEntry)
     {
@@ -2358,6 +2362,14 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
                                             &pFrm->QComVendorIE,
                                             psessionEntry);
 
+#ifdef WLAN_FEATURE_FILS_SK
+    if (lim_is_fils_connection(psessionEntry)) {
+        populate_dot11f_fils_params(pMac, pFrm, psessionEntry);
+        aes_block_size_len = AES_BLOCK_SIZE;
+    }
+#endif
+
+
     nStatus = dot11fGetPackedAssocRequestSize(pMac, pFrm, &nPayload);
     if (DOT11F_FAILED(nStatus))
     {
@@ -2374,7 +2386,11 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
              nStatus);
     }
 
-    nBytes = nPayload + sizeof(tSirMacMgmtHdr) + nAddIELen;
+    nBytes = nPayload + 
+            sizeof(tSirMacMgmtHdr) + nAddIELen;
+#ifdef WLAN_FEATURE_FILS_SK
+    nBytes += aes_block_size_len;
+#endif
 
     halstatus = palPktAlloc( pMac->hHdd, HAL_TXRX_FRM_802_11_MGMT,
             ( tANI_U16 )nBytes, ( void** ) &pFrame,
@@ -2447,7 +2463,6 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
         psessionEntry->assocReq = NULL;
         psessionEntry->assocReqLen = 0;
     }
-
     if( nAddIELen )
     {
         vos_mem_copy( pFrame + sizeof(tSirMacMgmtHdr) + nPayload,
@@ -2455,6 +2470,22 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
                       nAddIELen );
         nPayload += nAddIELen;
     }
+
+#ifdef WLAN_FEATURE_FILS_SK
+    if (lim_is_fils_connection(psessionEntry)) {
+        vos_status = aead_encrypt_assoc_req(pMac, psessionEntry,
+                                            pFrame, &nPayload);
+        if (!VOS_IS_STATUS_SUCCESS(vos_status)) {
+            limDiagEventReport(pMac,
+                        WLAN_PE_DIAG_ASSOC_IND_EVENT,
+                        psessionEntry, eSIR_FAILURE, eSIR_FAILURE);
+            palPktFree( pMac->hHdd, HAL_TXRX_FRM_802_11_MGMT,
+                        ( void* ) pFrame, ( void* ) pPacket );
+            vos_mem_free(pFrm);
+            return;
+        }
+    }
+#endif
 
     psessionEntry->assocReq = vos_mem_malloc(nPayload);
     if ( NULL == psessionEntry->assocReq )

MotoZ-2016/android_kernel_motorola_msm8996

CORE/MAC/src/pe/lim/limSendManagementFrames.c
@@ -52,7 +52,7 @@
 #ifdef WLAN_FEATURE_11W
 #include "wni_cfg.h"
 #endif
-
+#include<crypto/aes.h>
 #ifdef WLAN_FEATURE_VOWIFI_11R
 #include "limFTDefs.h"
 #endif
@@ -64,7 +64,7 @@
 #if defined WLAN_FEATURE_VOWIFI
 #include "rrmApi.h"
 #endif
-
+#include "qdf_crypto.h"
 #include "wlan_qct_wda.h"
 #include "lim_process_fils.h"
 
@@ -2071,6 +2071,8 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
     tANI_U8            *pIe = NULL;
     tANI_U32            ieLen = 0;
     tANI_U32            fixed_param_len = 0;
+    VOS_STATUS          vos_status;
+    tANI_U32            aes_block_size_len = 0;
 
     if(NULL == psessionEntry)
     {
@@ -2355,6 +2357,11 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
                                             &pFrm->QComVendorIE,
                                             psessionEntry);
 
+    if (lim_is_fils_connection(psessionEntry)) {
+        populate_dot11f_fils_params(pMac, pFrm, psessionEntry);
+        aes_block_size_len = AES_BLOCK_SIZE;
+    }
+
     nStatus = dot11fGetPackedAssocRequestSize(pMac, pFrm, &nPayload);
     if (DOT11F_FAILED(nStatus))
     {
@@ -2371,7 +2378,8 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
              nStatus);
     }
 
-    nBytes = nPayload + sizeof(tSirMacMgmtHdr) + nAddIELen;
+    nBytes = nPayload + sizeof(tSirMacMgmtHdr) 
+                      + nAddIELen + aes_block_size_len;
 
     halstatus = palPktAlloc( pMac->hHdd, HAL_TXRX_FRM_802_11_MGMT,
             ( tANI_U16 )nBytes, ( void** ) &pFrame,
@@ -2444,13 +2452,26 @@ limSendAssocReqMgmtFrame(tpAniSirGlobal   pMac,
         psessionEntry->assocReq = NULL;
         psessionEntry->assocReqLen = 0;
     }
-
     if( nAddIELen )
     {
         vos_mem_copy( pFrame + sizeof(tSirMacMgmtHdr) + nPayload,
                       pAddIE,
                       nAddIELen );
         nPayload += nAddIELen;
+    }
+
+    if (lim_is_fils_connection(psessionEntry)) {
+        vos_status = aead_encrypt_assoc_req(pMac, psessionEntry,
+                                            pFrame, &nPayload);
+        if (!VOS_IS_STATUS_SUCCESS(vos_status)) {
+            limDiagEventReport(pMac,
+                        WLAN_PE_DIAG_ASSOC_IND_EVENT,
+                        psessionEntry, eSIR_FAILURE, eSIR_FAILURE);
+            palPktFree( pMac->hHdd, HAL_TXRX_FRM_802_11_MGMT,
+                        ( void* ) pFrame, ( void* ) pPacket );
+            vos_mem_free(pFrm);
+            return;
+        }
     }
 
     psessionEntry->assocReq = vos_mem_malloc(nPayload);