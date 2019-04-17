2241a10b6a79124ffcee9c55583d56798b0d4ee8
Danct12 <danct12@disroot.org>
2019-04-17 12:04:58 +0700

    Fix power button not responding

--- drivers/platform/msm/qpnp-power-on.c
+++ drivers/platform/msm/qpnp-power-on.c
@@ -1423,7 +1423,7 @@ qpnp_pon_config_input(struct qpnp_pon *pon,  struct qpnp_pon_config *cfg)
 	}
 
 	/* don't send dummy release event when system resumes */
-	__set_bit(INPUT_PROP_NO_DUMMY_RELEASE, pon->pon_input->propbit);
+	//__set_bit(INPUT_PROP_NO_DUMMY_RELEASE, pon->pon_input->propbit); - Breaks power button!!
 	input_set_capability(pon->pon_input, EV_KEY, cfg->key_code);
 
 	return 0;
